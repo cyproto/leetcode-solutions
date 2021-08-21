1a9ef5b714197b23091f72c0853bba749959ec71
cyproto <kyashkarande@gmail.com>
2021-08-22 01:36:03 +0530

    ds_algo: cpp: Add diameter-of-binary-tree, find-all-numbers-disappeared-in-an-array

---
+++ ds_algo/cpp/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        int temp = 0;
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            temp = nums[i];
+            temp = temp > 0 ? temp : -temp;
+            if (nums[temp - 1] > 0)
+                nums[temp - 1] *= -1;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                res.push_back(i + 1);
+            }
+        }
+
+        return res;
+    }
+};
